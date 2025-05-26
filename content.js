85428bef80ec138b17ca9222fc91b50050af385f
Ethan Hackney <ethanhackney@pop-os.lan>
2025-05-26 13:24:55 -0700

    only use f when not a control key

--- content.js
+++ content.js
@@ -339,6 +339,8 @@ document.addEventListener("keydown", (e) => {
     return;
   }
   if (e.key === "f") {
+    if (e.ctrlKey)
+      return;
     e.preventDefault();
     e.stopPropagation();
     enterLinkMode();
